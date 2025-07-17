e8e32cea5017160737dd2e13df9549496f7b52c2
ji-mim <194115@jnu.ac.kr>
2025-07-17 16:13:43 +0900

    Feat: 페이지네이션 관련 테스트 추가 및 테스트 피드백 반영

--- src/test/java/gift/repository/MemberJpaRepositoryTest.java
+++ src/test/java/gift/repository/MemberJpaRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import gift.domain.Member;
 import gift.util.ShaUtil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,6 +18,7 @@ class MemberJpaRepositoryTest {
 
 
     @Test
+    @DisplayName("멤버 저장")
     void save() {
         String password = "123";
         String salt = ShaUtil.getSalt();
@@ -27,59 +29,77 @@ class MemberJpaRepositoryTest {
     }
 
     @Test
+    @DisplayName("ID로 멤버 조회 ")
     void findById() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
+        //given
         Member saveMember = repository.save(
                 new Member(null, "ex@email.com", encryptPassword, salt));
+        //then
         assertThat(repository.findById(saveMember.getId()).get()).isEqualTo((saveMember));
     }
 
     @Test
+    @DisplayName("이메일로 멤버 조회")
     void findByEmail() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
+        //given
         Member saveMember = repository.save(
                 new Member(null, "ex@email.com", encryptPassword, salt));
+        //then
         assertThat(repository.findByEmail(saveMember.getEmail()).get()).isEqualTo(saveMember);
     }
 
     @Test
+    @DisplayName("멤버 전체 조회")
     void findAll() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
+        //given
         Member saveMember1 = repository.save(
                 new Member(null, "ex1@email.com", encryptPassword, salt));
         Member saveMember2 = repository.save(
                 new Member(null, "ex2@email.com", encryptPassword, salt));
+        //then
         assertThat(repository.findAll().size()).isEqualTo(2);
     }
 
     @Test
+    @DisplayName("멤버 수정")
     void update() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
         Member saveMember = repository.save(
                 new Member(null, "ex@email.com", encryptPassword, salt));
-
+        //given
         saveMember.update("update@email.com", encryptPassword, salt);
-
+        //then
         assertThat(repository.findById(saveMember.getId()).get().getEmail()).isEqualTo(
                 "update@email.com");
     }
 
     @Test
+    @DisplayName("멤버 삭제")
     void delete() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
         Member saveMember1 = repository.save(
                 new Member(null, "ex1@email.com", encryptPassword, salt));
+        //given
         repository.deleteById(saveMember1.getId());
+        //then
         assertThat(repository.findAll().size()).isEqualTo(0);
 
     }
--- src/test/java/gift/repository/WishJpaRepositoryTest.java
+++ src/test/java/gift/repository/WishJpaRepositoryTest.java
@@ -7,10 +7,13 @@ import gift.domain.Member;
 import gift.domain.Product;
 import gift.domain.Wish;
 import gift.util.ShaUtil;
-import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @DataJpaTest
 class WishJpaRepositoryTest {
@@ -23,7 +26,9 @@ class WishJpaRepositoryTest {
     private ProductJpaRepository productRepository;
 
     @Test
+    @DisplayName("위시에 상품 저장")
     void save() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
@@ -33,12 +38,17 @@ class WishJpaRepositoryTest {
         Product savedProduct = productRepository.save(
                 new Product(null, "product1", 1000, "url.com"));
 
+        //given
         Wish savedWish = wishRepository.save(new Wish(null, member, savedProduct, 1));
+
+        //then
         assertThat(savedWish.getId()).isNotNull();
     }
 
     @Test
+    @DisplayName("멤버별로 상품 조회")
     void getWishListByMember() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
@@ -51,15 +61,50 @@ class WishJpaRepositoryTest {
         Product product2 = productRepository.save(
                 new Product(null, "product2", 1000, "url.com"));
 
+        //given
         wishRepository.save(new Wish(null, member, product1, 1));
         wishRepository.save(new Wish(null, member, product2, 1));
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Wish> wishes = wishRepository.findByMemberId(member.getId(), pageable);
 
-        List<Wish> members = wishRepository.findByMemberId(member.getId());
-        assertThat(members.size()).isEqualTo(2);
+        //then
+        assertThat(wishes.getContent().size()).isEqualTo(2);
     }
 
     @Test
+    @DisplayName("페이지네이션 테스트")
+    void 멤버별_상품조회_페이지네이션() {
+        //when
+        String password = "123";
+        String salt = ShaUtil.getSalt();
+        String encryptPassword = ShaUtil.encrypt(password, salt);
+        Member member = memberRepository.save(
+                new Member(null, "ex@email.com", encryptPassword, salt));
+
+        Product product1 = productRepository.save(
+                new Product(null, "product1", 1000, "url.com"));
+
+        Product product2 = productRepository.save(
+                new Product(null, "product2", 1000, "url.com"));
+
+        Product product3 = productRepository.save(
+                new Product(null, "product3", 1000, "url.com"));
+
+        //given
+        wishRepository.save(new Wish(null, member, product1, 1));
+        wishRepository.save(new Wish(null, member, product2, 1));
+        wishRepository.save(new Wish(null, member, product3, 1));
+        Pageable pageable = PageRequest.of(1, 2);
+        Page<Wish> wishes = wishRepository.findByMemberId(member.getId(), pageable);
+
+        //then
+        assertThat(wishes.getContent().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("수량 변경")
     void updateQuantity() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
@@ -70,12 +115,18 @@ class WishJpaRepositoryTest {
                 new Product(null, "product1", 1000, "url.com"));
 
         Wish savedWish = wishRepository.save(new Wish(null, member, savedProduct, 1));
+
+        //given
         savedWish.update(3);
+
+        //then
         assertThat(savedWish.getQuantity()).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("위시 상품 삭제")
     void delete() {
+        //when
         String password = "123";
         String salt = ShaUtil.getSalt();
         String encryptPassword = ShaUtil.encrypt(password, salt);
@@ -90,9 +141,14 @@ class WishJpaRepositoryTest {
 
         Wish wish1 = new Wish(null, member, product1, 1);
         Wish wish2 = new Wish(null, member, product2, 1);
+
+        //given
         wishRepository.save(wish1);
         wishRepository.save(wish2);
         wishRepository.deleteById(wish1.getId());
-        assertThat(wishRepository.findByMemberId(member.getId()).size()).isEqualTo(1);
+        Pageable pagable = PageRequest.of(0, 10);
+
+        //then
+        assertThat(wishRepository.findByMemberId(member.getId(), pagable).getContent().size()).isEqualTo(1);
     }
 }
